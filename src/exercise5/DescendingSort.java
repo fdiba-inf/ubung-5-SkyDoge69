44bf5fef9f0eb3cac57b905d3ae1559693c65e10
SkyDoge69 <luc2002penta@gmail.com>
2021-11-11 09:56:14 +0200

    Forth try

--- src/exercise5/DescendingSort.java
+++ src/exercise5/DescendingSort.java
@@ -17,7 +17,17 @@ public class DescendingSort {
             numbers[index] = input.nextInt();
         }
 
-        // Sort numbers in descending order
+        int temp = 0;
+        for (int i = 0; i < numbers.length - 1; i++) {
+            for (int j = 0; j < numbers.length - i - 1; j++) {
+                if (numbers[j] < numbers[j + 1]) {
+                    temp = numbers[j];
+                    numbers[j] = numbers[j + 1];
+                    numbers[j + 1] = temp;
+                }
+            }
+        }
+
 
         String numbersAsString = Arrays.toString(numbers);
         System.out.println("Sorted numbers: " + numbersAsString);
